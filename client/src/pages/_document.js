9a138d82c2f2be517b37611bdd8b7d94f4c4010f
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-02-26 02:37:35 -0600

    added meta and links to head

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head></Head>
+			<Head>
+				<meta name="apple-mobile-web-app-title" content="Lifefind" />
+
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta charSet="UTF-8" />
+
+				<link rel="shortcut icon" href="/lifefind-orange.svg" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/180fav.png" />
+				<link rel="icon" type="image/png" sizes="192x192" href="/favicons/192fav.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/32fav.png" />
+				{/* <link rel="icon" type="image/png" sizes="16x16" href="/favicons/16fav.png" /> */}
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
